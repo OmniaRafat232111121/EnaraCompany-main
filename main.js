f94ccbf4e1682ed24464f993fe9bf766a0a81818
omnia rafat <76200523+OmniaRafat232111121@users.noreply.github.com>
2023-05-14 17:50:47 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,45 @@
+let navbar = document.querySelector('.header .navbar');
+let contactInfo = document.querySelector('.contact-info');
+
+document.querySelector('#info-btn').onclick = () => {
+    contactInfo.classList.add('active');
+}
+document.querySelector('#close-contact-info').onclick = () => {
+    contactInfo.classList.remove('active');
+}
+document.querySelector('#menu-btn').onclick = () => {
+    navbar.classList.toggle('active');
+}
+//light Gallery
+lightGallery(document.querySelector('.projects .boxx-container'));
+//swiper
+var swiper = new Swiper(".home-slider", {
+       loop: true,
+       grabCursor:true,
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+});
+      
+
+
+var swiper = new Swiper(".logo-slider", {
+   loop:true,
+   grabCursor:true,
+   spaceBetween: 20,
+    breakpoints: {
+      450: {
+         slidesPerView: 2,
+       },
+      640: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 4,
+      },
+      1000: {
+        slidesPerView: 5,
+      },
+   },
+});
